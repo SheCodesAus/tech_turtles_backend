23d3bffebe4a91547bb29a860f977af186256cad
jennyguidetti <j_guidetti95@hotmail.com>
2025-01-20 16:48:19 +1000

    added model to items

--- presentpals/items/models.py
+++ presentpals/items/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+STATUS = [
+    ('incomplete', 'Incomplete'),
+    ('in_progress', 'In Progress'),
+    ('complete', 'Complete')
+]
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    cost = models.IntegerField()
+    where_to_buy = models.TextField(blank=True)
+    notes = models.TextField()
+    status = models.CharField(
+        max_length=15,
+        choices=STATUS,
+        default='incomplete',
+    )
+    is_open = models.BooleanField()
+    date_created = models.DateTimeField(auto_now_add=True)
