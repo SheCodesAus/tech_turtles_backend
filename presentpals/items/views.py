92c2e1328a4737a7ca304ab8be8a701a0eb03894
jennyguidetti <j_guidetti95@hotmail.com>
2025-01-20 16:58:42 +1000

    added serializer to items and views

--- presentpals/items/views.py
+++ presentpals/items/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+from .models import Item
+from .serializers import ItemSerializer
 
-# Create your views here.
+class ItemList(APIView):
+
+    def get(self, request):
+        items = Item.objects.all()
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+class ItemDetail(APIView):
+    def get_object(self, pk):
+        try:
+            item = Item.objects.get(pk=pk)
+            return item
+        except Item.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        item = self.get_object(pk)
+        serializer = ItemSerializer(item)
+        return Response(serializer.data)
