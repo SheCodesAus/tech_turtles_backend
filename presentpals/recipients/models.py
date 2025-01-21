18a8f6a1e34103cdcde710e65ed1bcd51428468a
jennyguidetti <j_guidetti95@hotmail.com>
2025-01-21 20:15:52 +1000

    updated recipients app with UUID field

--- presentpals/recipients/models.py
+++ presentpals/recipients/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from lists.models import List
+import uuid
 
 class Recipient(models.Model):
     name = models.CharField(max_length=200)
@@ -9,4 +10,9 @@ class Recipient(models.Model):
         'lists.List',
         on_delete=models.CASCADE,
         related_name='recipients'
+    )
+    unique_code = models.UUIDField(
+        unique=True,
+        editable=False,
+        default = uuid.uuid4
     )
