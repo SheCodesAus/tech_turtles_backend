7083d6124cab8cce222b6f6bafc41852c821ccc2
jennyguidetti <j_guidetti95@hotmail.com>
2025-01-22 22:13:14 +1000

    added permission class to POST lists

--- presentpals/lists/views.py
+++ presentpals/lists/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from django.http import Http404
 from .models import List
 from .serializers import ListSerializer, ListDetailSerializer, RecipientSerializer
 
 class ListList(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
     def get(self, request):
         lists = List.objects.all()
         serializer = ListSerializer(lists, many=True)
