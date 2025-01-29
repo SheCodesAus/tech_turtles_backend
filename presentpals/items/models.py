a5e61b78aa00947a7676fc880ec8c2c95fc55f7b
jennyguidetti <j_guidetti95@hotmail.com>
2025-01-29 11:41:47 +1000

    added due date, priority and delivery status to items db

--- presentpals/items/models.py
+++ presentpals/items/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from recipients.models import Recipient
 
+PRIORITY = [
+    (1, 'High'),
+    (2, 'Medium'),
+    (3, 'Low'),
+]
+
+DELIVERY_STATUS = [
+    ('pending', 'Pending'),
+    ('shipped', 'Shipped'),
+    ('delivered', 'Delivered'),
+    ('na', 'Not Applicable'),
+]
+
 STATUS = [
     ('incomplete', 'Incomplete'),
     ('complete', 'Complete')
@@ -10,11 +23,22 @@ class Item(models.Model):
     cost = models.PositiveIntegerField()
     where_to_buy = models.TextField(blank=True)
     notes = models.TextField(blank=True)
+    priority = models.IntegerField(
+        choices=PRIORITY,
+        default=2,
+        blank=True,
+    )
+    delivery_status = models.CharField(
+        max_length=15,
+        choices=DELIVERY_STATUS,
+        default='na',
+    )
     status = models.CharField(
         max_length=15,
         choices=STATUS,
         default='incomplete',
     )
+    due_date = models.DateField(null=True, blank=True)
     is_open = models.BooleanField(default=True)
     date_created = models.DateTimeField(auto_now_add=True)
     recipient = models.ForeignKey(
